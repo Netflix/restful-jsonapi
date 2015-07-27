31eccf357c02e5ad603b6aadae53567761497312
Aram Grigoryan <agrigoryan@netflix.com>
2015-07-27 16:49:57 -0700

    Added serializable errors

--- lib/restful/jsonapi.rb
+++ lib/restful/jsonapi.rb
@@ -1,6 +1,7 @@
 require "restful/jsonapi/version"
 require 'restful/jsonapi/railtie' if defined?(Rails)
 require 'restful/jsonapi/restify_param'
+require 'restful/jsonapi/serializable_errors'
 require 'restful/jsonapi/active_model_serializer'
 
 module Restful
--- lib/restful/jsonapi/railtie.rb
+++ lib/restful/jsonapi/railtie.rb
@@ -5,8 +5,12 @@ module Restful
         ActiveSupport.on_load(:action_controller) do
           puts "Extending #{self} with Restful::Jsonapi::RestifyParam"
           include Restful::Jsonapi::RestifyParam
+
+          puts "Extending #{self} with Restful::Jsonapi::SerializableErrors"
+          include Restful::Jsonapi::SerializableErrors
         end
       end
+
       initializer "restful-jsonapi.active_model_serializer" do
         ActiveModel::Serializer.class_eval do
           puts "Extending #{self} with Restful::Jsonapi::ActiveModelSerializer"
