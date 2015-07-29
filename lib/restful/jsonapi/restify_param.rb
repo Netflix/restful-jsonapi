15498ee425c7b3bd38ee01338e9b866d5484ca5f
Zach Wentz <zkwentz@gmail.com>
2015-07-29 16:38:03 -0700

    update to jsonapi spec

--- lib/restful/jsonapi/restify_param.rb
+++ lib/restful/jsonapi/restify_param.rb
@@ -11,34 +11,32 @@ module Restful
 
       def restify_value(param_key, value = params)
         if value == params
-          value = params.clone
+          value = params.clone[:data]
         end
-        if value[:data]
-          if value[:data].delete(:type).underscore == param_key.to_s.pluralize
-            new_params = ActionController::Parameters.new
-            attributes = value[:data].has_key?(:attributes) ? value[:data][:attributes] : value[:data]
-            attributes.merge!(id: value[:data][:id]) if value[:data][:id]
-            attributes.transform_keys!(&:underscore)
-            new_params.merge!(attributes)
-            if value[:data].has_key? :relationships
-              value[:data][:relationships].each do |k,v|
-                if k.pluralize != k
-                  unless v[:data].nil?
-                    new_params["#{k}_id"] = v[:data][:id]
-                  else
-                    new_params["#{k}_id"] = nil
-                  end
+        if value.delete(:type).underscore == param_key.to_s.pluralize
+          new_params = ActionController::Parameters.new
+          attributes = value.has_key?(:attributes) ? value[:attributes] : value
+          attributes.merge!(id: value[:id]) if value[:id]
+          attributes.transform_keys!(&:underscore)
+          new_params.merge!(attributes)
+          if value.has_key? :relationships
+            value[:relationships].each do |k,v|
+              if k.pluralize != k
+                unless v[:data].nil?
+                  new_params["#{k}_id"] = v[:data][:id]
                 else
-                  relationship_key = k.to_s.underscore+"_attributes"
-                  new_params[relationship_key] ||= []
-                  v[:data].each do |vv|
-                    new_params[relationship_key].push restify_value(k,vv)
-                  end unless v[:data].nil?
+                  new_params["#{k}_id"] = nil
                 end
+              else
+                relationship_key = k.to_s.underscore+"_attributes"
+                new_params[relationship_key] ||= []
+                v[:data].each do |vv|
+                  new_params[relationship_key].push restify_value(k,vv)
+                end unless v[:data].nil?
               end
             end
-            new_params
           end
+          new_params
         end
       end
 
