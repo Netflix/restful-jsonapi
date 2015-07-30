0c599bcb9e31a95bd36ed8e7a83540d721562682
Zach Wentz <zkwentz@gmail.com>
2015-07-29 23:01:24 -0700

    update restify param logic

--- lib/restful/jsonapi/restify_param.rb
+++ lib/restful/jsonapi/restify_param.rb
@@ -22,23 +22,29 @@ module Restful
           if value.has_key? :relationships
             value[:relationships].each do |k,v|
               if k.pluralize != k
+
+              # belongs to relationship
                 unless v[:data].nil?
                   new_params["#{k}_id"] = v[:data][:id]
                 else
                   new_params["#{k}_id"] = nil
                 end
-              elsif v[:attributes].present?
-                relationship_key = k.to_s.underscore+"_attributes"
-                new_params[relationship_key] ||= []
-                v[:data].each do |vv|
-                  new_params[relationship_key].push restify_value(k,vv)
-                end unless v[:data].nil?
-              else
-                relationship_key = k.to_s.singularize.underscore+"_ids"
-                new_params[relationship_key] ||= []
-                v[:data].each do |vv|
-                  new_params[relationship_key].push vv[:id]
-                end unless v[:data].nil?
+
+              # has many relationship
+              elsif v[:data].present?
+                if v[:data].reject{|data|data.has_key? :attributes}.blank?
+                  relationship_key = k.to_s.underscore+"_attributes"
+                  new_params[relationship_key] ||= []
+                  v[:data].each do |vv|
+                    new_params[relationship_key].push restify_value(k,vv)
+                  end unless v[:data].nil?
+                else
+                  relationship_key = k.to_s.singularize.underscore+"_ids"
+                  new_params[relationship_key] ||= []
+                  v[:data].each do |vv|
+                    new_params[relationship_key].push vv[:id]
+                  end unless v[:data].nil?
+                end
               end
             end
           end
--- lib/restful/jsonapi/version.rb
+++ lib/restful/jsonapi/version.rb
@@ -1,5 +1,5 @@
 module Restful
   module Jsonapi
-    VERSION = "0.0.7"
+    VERSION = "0.0.8"
   end
 end
