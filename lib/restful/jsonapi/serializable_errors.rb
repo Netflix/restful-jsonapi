c1030c25b2703f0902a9f4aecd63a3fb14357b5d
Jearvon Dharrie <j.dharrie@gmail.com>
2016-01-12 23:08:03 -0500

    Small refactor
    Remove temp json variable

--- lib/restful/jsonapi/serializable_errors.rb
+++ lib/restful/jsonapi/serializable_errors.rb
@@ -7,17 +7,13 @@ module Restful
         errors = object.errors
         prefix = object.class.to_s.demodulize.underscore
 
-        json = {}
-
         new_hash = errors.to_hash.map do |k, v|
           v.map do |msg|
             { id: "#{prefix}.#{k}", title: msg }
           end
         end.flatten
 
-        json[:errors] = new_hash
-
-        json
+        { errors: new_hash }
       end
     end
   end
