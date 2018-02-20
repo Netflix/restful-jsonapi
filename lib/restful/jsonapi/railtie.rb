6d070235e325257d0c6a6535b6c9736ae50badfe
Marten <mail@martenschilstra.nl>
2018-02-20 13:09:24 +0100

    Silence this gem

--- lib/restful/jsonapi/railtie.rb
+++ lib/restful/jsonapi/railtie.rb
@@ -3,17 +3,13 @@ module Restful
     class Railtie < Rails::Railtie
       initializer "restful-jsonapi.action_controller" do
         ActiveSupport.on_load(:action_controller) do
-          puts "Extending #{self} with Restful::Jsonapi::RestifyParam"
           include Restful::Jsonapi::RestifyParam
-
-          puts "Extending #{self} with Restful::Jsonapi::SerializableErrors"
           include Restful::Jsonapi::SerializableErrors
         end
       end
 
       initializer "restful-jsonapi.active_model_serializer" do
         ActiveModel::Serializer.class_eval do
-          puts "Extending #{self} with Restful::Jsonapi::ActiveModelSerializer"
           include Restful::Jsonapi::ActiveModelSerializer
         end
       end if defined? ActiveModel::Serializer
